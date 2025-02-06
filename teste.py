a6a381806a26440c90ce3f05257264b59345fcfc
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-05 21:22:32 -0300

    calcular o triplo

--- teste.py
+++ teste.py
@@ -1,21 +1,32 @@
 import tkinter as tk
 
-def calcular_dobro():
+def calcular_tripo():
     try:
         numero = int(entrada.get())
-        resultado = numero * 2
-        label_resultado.config(text=f"O dobro de {numero} é {resultado}.")
+        resultado = numero * 3
+        label_resultado.config(text=f"O triplo de {numero} é {resultado}.")
     except ValueError:
-        label_resultado.config(text="Por favor, instira um número válido.")        
-
+        label_resulatdo.config(text="Por favor, insira um número válido.")        
+        
 janela = tk.Tk()
-janela.title("Calculadora de Dobro")        
-janela.geometry("400x300")
+janela.title("Calculadora de Triplo")        
+
+largura = 400
+altura = 200
+largura_tela = janela.winfo_screenwidth()
+altura_tela = janela.winfo_screenheight()
+x = (largura_tela // 2) - (largura // 2)
+y = (altura_tela // 2) - (altura // 2)
+
+janela.geometry(f"{largura}x{altura}+{x}+{y}")
+
+label_instrucao = tk.Label(janela, text="Digite um número para calcular o triplo: ", font=("Arial", 12))
+label_instrucao.pack(pady=10)
 
 entrada = tk.Entry(janela, font=("Arial", 14))
-entrada.pack(pady=10)
+entrada.pack(pady=10)  
 
-botao_calcular = tk.Button(janela, text="Calcular", command=calcular_dobro)
+botao_calcular = tk.Button(janela, text="Calcular", command=calcular_tripo)
 botao_calcular.pack(pady=10)
 
 label_resultado = tk.Label(janela, text="", font=("Arial", 14))
